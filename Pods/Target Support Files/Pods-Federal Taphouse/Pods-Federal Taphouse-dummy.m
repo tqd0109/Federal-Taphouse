1a933ba27b3c72cf2b8f3fa2f8acef4bef2a43bf
Trung Do <doquoctrung93@gmail.com>
2016-09-06 12:56:51 -0400

    initial commit

---
+++ Pods/Target Support Files/Pods-Federal Taphouse/Pods-Federal Taphouse-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Federal_Taphouse : NSObject
+@end
+@implementation PodsDummy_Pods_Federal_Taphouse
+@end
---
+++ Pods/Target Support Files/Pods-Federal Taphouse/Pods-Federal Taphouse-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Federal_TaphouseVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Federal_TaphouseVersionString[];
+
