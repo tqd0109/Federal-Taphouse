de30d58b6f703c5829beb8e38ecbf5e9a2c56a82
Trung Do <doquoctrung93@gmail.com>
2016-09-08 13:50:10 -0400

    invite people through sms implemented

--- Federal Taphouse/Invite.swift
+++ Federal Taphouse/Invite.swift
@@ -7,10 +7,34 @@
 //
 
 import Foundation
+import MessageUI
 
-class Invite: UIViewController{
+class Invite: UIViewController, MFMessageComposeViewControllerDelegate{
+    
+    @IBOutlet var phoneNumber: UITextField!
     override func viewDidLoad() {
         self.view.addGestureRecognizer(revealViewController().panGestureRecognizer())
+        self.hideKeyboardWhenTappedAround()
 
     }
+    
+    @IBAction func sendText(sender: AnyObject) {
+        if (MFMessageComposeViewController.canSendText()) {
+            let controller = MFMessageComposeViewController()
+            controller.body = "Message Body"
+            controller.recipients = [phoneNumber.text!]
+            controller.messageComposeDelegate = self
+            self.presentViewController(controller, animated: true, completion: nil)
+        }
+    }
+    
+    func messageComposeViewController(controller: MFMessageComposeViewController!, didFinishWithResult result: MessageComposeResult) {
+        //... handle sms screen actions
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        self.navigationController?.navigationBarHidden = false
+    }
+    
 }
